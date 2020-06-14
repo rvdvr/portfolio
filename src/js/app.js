1b7ecdae59a7911f5d58d46c7c74929194529e42
Andrey Radantsev <avradantsev@yandex.ru>
2020-06-14 03:01:08 +0300

    Главная страница готова

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,42 @@
+$(document).ready( function() {
 
-const emptyObject = () => 'ES6!';
+  (function() {
+    $('.contacts-form__radio-btn').on('click', function() {
+      var $this = $(this);
+  
+      if ($this.attr('id') === 'gmail') {
+        $('#phone').css('display', 'none');
+        $('#email').css('display', 'block');
+      } else {
+        $('#email').css('display', 'none');
+        $('#phone').css('display', 'block');
+      }
+    });
+  })();
 
-emptyObject();
+  (function() {
+    $('#phone input').inputmask({
+      mask: '+7 (999) 999-99-99'
+    });
+
+    $.validator.messages = {
+      required: 'Заполните поле',
+      phone: 'Введите телефон полностью'
+    }
+
+    $.validator.addMethod('validPhone', function(value, element) {
+      return /\+\d{1} \(\d{3}\) \d{3}-\d{2}-\d{2}/g.test(value);
+    }, $.validator.messages.phone);
+
+    $('.contacts__form').validate({
+      rules: {
+        ph: {
+          validPhone: true
+        }
+      },
+      submitHandler: function(form) {
+        alert('Сообщение отправлено');
+      }    
+    })
+  })();
+})
